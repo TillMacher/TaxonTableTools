0db6e3cc7f924c1319b1f3ee6f5d7d6d830aba35
Till Macher <tillmacher@gmx.de>
2020-07-13 17:09:43 +0200

    v1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="taxontabletools", # Replace with your own username
+    name="taxontabletools-TillMacher", # Replace with your own username
     version="1.0.0",
     author="Till-Hendrik Macher",
     author_email="till-hendrik.macher@uni-due.de",
